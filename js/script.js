1c02868ec6deb8274d14f96e1ef371c85b719376
Tymofii <159185555+tymofii-stepanov@users.noreply.github.com>
2024-04-04 18:17:25 +0000

    Done work

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
+// Created by: Tymofii
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+
+"use strict"
+
+/**
+ * This function calculates Volume of Pyramid
+ */
+function calculateVolumeOfPyramid() {
+  // input
+  const length = parseInt(document.getElementById('length-mm').value)
+  const width = parseInt(document.getElementById('width-mm').value)
+  const height = parseInt(document.getElementById('height-mm').value)
+
+
+  // process
+  const VolumeOfPyramid = Math.floor((length * width * height) / 3);
+
+  // output
+  document.getElementById('volume').innerHTML = 'Volume is ' +  VolumeOfPyramid + ' mm'
+}
